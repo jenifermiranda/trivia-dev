e2df30268b81d2c842abcde82cce70c235a4fc60
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2023-12-29 17:43:39 +0000

    remove: apaga o componente Game e substitui pelo GameCopy

--- trivia-dev-frontend/src/App.tsx
+++ trivia-dev-frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import Game from './pages/Game';
-import GameCopy from './pages/GameCopy';
 import Login from './pages/Login';
 import UserResults from './pages/UserResults';
 import GlobalProvider from './context/GlobalProvider';
+import Game from './pages/Game';
 
 function App() {
   useEffect(() => {
@@ -15,7 +14,7 @@ function App() {
     <GlobalProvider>
       <Routes>
         <Route path="/" element={ <Login /> } />
-        <Route path="/game" element={ <GameCopy /> } />
+        <Route path="/game" element={ <Game /> } />
         <Route path="/results" element={ <UserResults /> } />
       </Routes>
     </GlobalProvider>
--- trivia-dev-frontend/src/pages/Game.tsx
+++ trivia-dev-frontend/src/pages/Game.tsx
@@ -1,56 +1,41 @@
-import { useEffect, useState } from 'react';
-import Header from '../components/Header';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { fetchQuestionsAPI } from '../services/fetchAPI';
+import GlobalContext from '../context/GlobalContext';
+import Header from '../components/Header';
 import Question from '../types/Question.type';
 import Options from '../types/Options.type';
 import Answer from '../types/Answer.type';
 import Level from '../types/Level.type';
 
 function Game() {
+  const navigate = useNavigate();
+
+  // Estados globais - Aplicação
+  const { userScore, setUserScore } = useContext(GlobalContext);
+  const { questions, setQuestions } = useContext(GlobalContext);
+  const { allQuestions, setAllQuestions } = useContext(GlobalContext);
+  const { rightAnswers, setRightAnswers } = useContext(GlobalContext);
+
+  // Estados Locais - Pagina Game
   const [questionIndex, setQuestionIndex] = useState(0);
   const [actualQuestion, setActualQuestion] = useState<Question>();
-  const [questions, setQuestions] = useState<Question[]>([]);
   const [randomOptions, setRandomOptions] = useState<Options[]>();
-  // const [redirect, setRedirect] = useState(false);
-  const [counter, setCounter] = useState<number>(10);
+  const [counter, setCounter] = useState<number>(30);
   const [showAnswer, setShowAnswer] = useState<boolean>(false);
   const [allAnswers, setAllAnswers] = useState<boolean>(false);
+  const [errorAPI, setErrorAPI] = useState<boolean>(false);
+  const [timeAnswer, setTimeAnswer] = useState(0);
 
-  // faz uma requisição a uma API
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { results } = await fetchQuestionsAPI();
-        if (results.length > 0) {
-          setQuestions(results);
-          setActualQuestion(results[questionIndex]);
-          setRandomOptions(getRandomOptions(results[questionIndex]));
-        }
-      } catch (error) {
-        console.log('Erro ao buscar perguntas.', error);
-      }
-    };
+  // Embaralha as alternativas
+  const shuffleArray = (optionsAnswers: Options[]): Options[] => {
+    const NEGATIVE_NUM = -1;
+    const shuffleOptions = optionsAnswers
+      .sort(() => Math.random() + Math.random() * NEGATIVE_NUM);
+    return shuffleOptions;
+  };
 
-    fetchData();
-  }, [questionIndex]);
-
-  // contador
-  //   useEffect(() => {
-  //     const time = setInterval(() => {
-  //       setCounter((prevCounter) => (prevCounter > 0 ? prevCounter - 1 : 0));
-  //       setAllAnswers((prevAllAnswers) => {
-  //         if (counter === 0 || showAnswer) {
-  //           clearInterval(time);
-  //           return true;
-  //         }
-  //         return prevAllAnswers;
-  //       });
-  //     }, 1000);
-
-  //     return () => clearInterval(time);
-  //   }, [counter, showAnswer]);
-
-  // retorna as alternativas certas e erradas
+  // Retorna as alternativas certas e erradas ordenadas aleatoriamente
   const getRandomOptions = (question: Question): Options[] => {
     const options: Options[] = [
       {
@@ -68,83 +53,128 @@ function Game() {
     return shuffleArray(options);
   };
 
-  // randomiza questoes
-  function shuffleArray<T>(array: T[]) {
-    const NEGATIVE_NUM = -1;
-    const shuffleOptions = array.sort(() => Math.random() + Math.random() * NEGATIVE_NUM);
-    // retorna o msm array embaralhado
-    return shuffleOptions;
-  }
-
-  // atualiza questao
-  const getActualQuestion = (questions: Question[]) => {
-    const currQuestion = questions[questionIndex];
-    const randomOptions = getRandomOptions(currQuestion);
+  // Atualiza questao
+  const getActualQuestion = (questionsAPI: Question[]) => {
+    const currQuestion = questionsAPI[questionIndex];
+    const newRandomOptions = getRandomOptions(currQuestion);
     setActualQuestion(currQuestion);
-    setRandomOptions(randomOptions);
+    setRandomOptions(newRandomOptions);
+    setQuestionIndex((prevQuestionIndex) => prevQuestionIndex + 1);
   };
 
-  // atualiza a pontuacao
+  // Executa requisição a API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { results } = await fetchQuestionsAPI();
+        if (results.length > 0) {
+          setAllQuestions((prevAllQuestions) => [...prevAllQuestions,
+            ...results.slice(0, (results.length - 1))]);
+          setQuestions(results);
+          getActualQuestion(results);
+        }
+      } catch (error) {
+        setErrorAPI(true);
+        console.log('Erro ao buscar perguntas.', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Temporizador - 30s
+  useEffect(() => {
+    const time = setInterval(() => {
+      setCounter((prevCounter) => (prevCounter > 0 ? prevCounter - 1 : 0));
+      setAllAnswers((prevAllAnswers) => {
+        if (counter === 0 || showAnswer) {
+          clearInterval(time);
+          return true;
+        }
+        return prevAllAnswers;
+      });
+    }, 1000);
+
+    return () => clearInterval(time);
+  }, [counter, showAnswer]);
+
+  // Atualiza a pontuacao
   const updateScore = (answer: Answer) => {
     if (answer.answer === 'right') {
       const points = 10;
       const level: Record<Level, number> = { easy: 1, medium: 2, hard: 3 };
       const score = points + counter * Number(level[answer.level]);
-      console.log(score);
-      // dispatch(updateScore(score));
+      setUserScore(score + userScore);
+      setRightAnswers((prevRightAnswers) => [...prevRightAnswers, answer]);
     }
     setShowAnswer(true);
+    setTimeAnswer(counter);
   };
 
-  // funcao do botao PROXIMO
+  // Função do botao PROXIMO
   const handleClick = () => {
-    setQuestionIndex((prevQuestionIndex) => prevQuestionIndex + 1);
+    if (questionIndex === questions.length - 1) {
+      navigate('/results');
+    }
+    getActualQuestion(questions);
     setShowAnswer(false);
     setAllAnswers(false);
-    // setActualQuestion(true);
-    setCounter(10);
-
-    // getActualQuestion(questions);
+    setCounter(30);
   };
 
   return (
     <section>
       <Header />
-      <h2>
-        Tempo:
-        {' '}
-        {counter}
-      </h2>
-      {actualQuestion && (
+      {errorAPI ? <h2>Erro ao buscar perguntas. Tente novamente mais tarde!</h2> : (
         <div>
-          <h2>{actualQuestion.category}</h2>
-          <h3>{actualQuestion.difficulty}</h3>
-          <h3>{actualQuestion.question}</h3>
-          <div>
-            {randomOptions && randomOptions.map((option, index) => {
-              const isRight = option.answer === 'right';
-              const color = showAnswer
-                ? (isRight ? 'green' : 'red')
-                : 'white';
-              return (
-                <button
-                  key={ index }
-                  onClick={ () => updateScore(option) }
-                  style={ { backgroundColor: color } }
-                  disabled={ allAnswers }
-                >
-                  {option.value}
-                </button>
-              );
-            })}
-          </div>
+          <h2>
+            Tempo:
+            {' '}
+            {showAnswer ? timeAnswer : counter}
+          </h2>
+          {actualQuestion && (
+            <div>
+              <h2>
+                Questão:
+                {' '}
+                {questionIndex}
+              </h2>
+              <h2>{actualQuestion.category}</h2>
+              <h3>{actualQuestion.difficulty}</h3>
+              <h3>{actualQuestion.question}</h3>
+              <div>
+                {randomOptions && randomOptions.map((option, index) => {
+                  const isRight = option.answer === 'right';
+                  let color = 'white';
+                  if (showAnswer) {
+                    color = isRight ? 'green' : 'red';
+                  }
+                  return (
+                    <button
+                      key={ index }
+                      onClick={ () => updateScore(option) }
+                      style={ { backgroundColor: color } }
+                      disabled={ allAnswers }
+                    >
+                      {option.value}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+          <button
+            onClick={ handleClick }
+            disabled={ !allAnswers }
+          >
+            {
+          questionIndex === questions.length - 1
+            ? 'Ver resultados'
+            : 'Próxima'
+        }
+          </button>
         </div>
       )}
-      <button
-        onClick={ handleClick }
-      >
-        Próxima
-      </button>
     </section>
   );
 }
