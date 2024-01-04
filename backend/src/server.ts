7b7797575ae6323ec55da0dc90527529e6e27487
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2024-01-04 20:10:31 -0300

    banco funcionando

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,12 @@
 import app from './app';
+import { getAllQuestions } from './services/questions.service';
 
 const PORT = 3001;
 const server = app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+
+app.get('/', async (req, res) => {
+    const result = await getAllQuestions();
+    res.json(result);
+});
 
 export default server;
