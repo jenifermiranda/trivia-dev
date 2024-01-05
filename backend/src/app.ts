6fdba8aba48a6de23c592658edc1014c216295ff
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2024-01-05 18:09:22 -0300

    installing cors on backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import questionsRouter from './routers/questions.router';
 
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(questionsRouter);
